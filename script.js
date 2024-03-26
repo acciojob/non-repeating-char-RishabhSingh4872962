e895b38f110bb33dca5804848e6c97a8ead2124f
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-26 09:15:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if (str.length==1) {
+		return str;
+	}
+	for (let i = 0; i < str.length; i++) {
+		let s1=str.charAt(i);
+		let s2=str.charAt(i+1);
+		
+		let repeat=1;
+		if (s1==s2) {
+			repeat++;
+			i++;
+		}
+		if (repeat==1 && str.charAt(str.length-1)!=str.charAt(str.length-2)) {
+			return s1;
+		}
+	}
+	return "null"; 
 }
+
+// console.log(firstNonRepeatedChar("aabbcdd"))
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
